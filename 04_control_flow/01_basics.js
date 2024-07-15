25a064e1772d6e901e5ab19c0ee2221459ef10b2
bigyan-sapkota <bigyansapkotacodes@gmail.com>
2024-07-15 22:48:31 +0545

    learnt control flow and iteration basics

---
+++ 04_control_flow/01_basics.js
@@ -0,0 +1,57 @@
+// if
+// if (true) {
+// }
+
+const isUserLoggedIn = true;
+const temperature = 41;
+
+// if (temperature === 40) {
+//   console.log("Temperature is less than 50");
+// } else {
+//   console.log("Temperature is greater than 50");
+// }
+
+// console.log("Execute");
+
+// >, <, >=, <=, ==, !=,  ===
+
+// const score = 200;
+
+// if (score > 100) {
+//   const power = "fly";
+//   console.log(`Power of user : ${power}`);
+// }
+
+// console.log(`Power of user : ${power}`);
+
+// const balance = 1000;
+
+// if (balance >= 1000) {
+//   console.log("Isn't greater than  1000");
+// }
+
+// if (balance > 500) console.log("test"), console.log("test 2");
+
+// if (balance < 500) {
+//   console.log("Less than 500");
+// } else if (balance < 750) {
+//   console.log("Less than 750");
+// } else if (balance < 900) {
+//   console.log("Less than 900");
+// } else {
+//   console.log("Less than 1200");
+// }
+
+// const userLoggedIn = true;
+// const debitCard = true;
+
+// const loggedInFromGoogle = false;
+// const loggedInFromEmail = true;
+
+// if (userLoggedIn && debitCard) {
+//   console.log("You can shop");
+// }
+
+// if (loggedInFromEmail || loggedInFromGoogle) {
+//   console.log("User logged in");
+// }
---
+++ 04_control_flow/03_truthy.js
@@ -0,0 +1,42 @@
+// const userEmail = [];
+
+// if (userEmail) {
+//   console.log("Got user email");
+// } else {
+//   console.log("Don't have user email");
+// }
+
+// FALSY VALUES------------------------------
+// 1) false
+// 2) 0
+// 3) -0
+// 4) BigInt 0n
+// 5) ""
+// 6) null
+// 7) undefined
+// 8) NaN
+
+// TRUTHY VALUES ---------------------------
+// true, "0", "false", " ", [], {}, function(){}
+
+// if (userEmail.length === 0) {
+//   console.log("Array is empty");
+// }
+
+// const emptyObj = {};
+
+// if (Object.keys(emptyObj).length === 0) {
+//   console.log("Object is empty");
+// }
+
+// Nullish Coalescing Operator(??) : null undefined
+// let val1;
+// val1 = 5 ?? 10;
+// val1 = null ?? 10;
+// val1 = undefined ?? 10;
+// val1 = null ?? 10 ?? 20;
+// console.log(val1);
+
+// Ternary Operator
+// const iceTeaPrice = 100;
+// iceTeaPrice <= 80 ? console.log("Less than 80") : console.log("More than 80");
